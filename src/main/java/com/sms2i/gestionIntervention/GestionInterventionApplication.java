400314264d48e32936408d1a8ce29168b4c58168
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-06-30 15:33:34 +0100

    intial

---
+++ src/main/java/com/sms2i/gestionIntervention/GestionInterventionApplication.java
@@ -0,0 +1,13 @@
+package com.sms2i.gestionIntervention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionInterventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionInterventionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sms2i/gestionIntervention/GestionInterventionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms2i.gestionIntervention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionInterventionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
