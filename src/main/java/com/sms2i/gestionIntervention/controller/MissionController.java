dcad1797f166359b46453ddac0d2ad0955cd6ab7
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-07-01 15:38:26 +0100

    generic service and model

--- src/main/java/com/sms2i/gestionIntervention/controller/MissionController.java
+++ src/main/java/com/sms2i/gestionIntervention/controller/MissionController.java
@@ -1,7 +1,8 @@
-/** package com.sms2i.gestionIntervention.controller;
+ package com.sms2i.gestionIntervention.controller;
 
 
 import com.sms2i.gestionIntervention.model.Mission;
+import com.sms2i.gestionIntervention.service.GenericService;
 import com.sms2i.gestionIntervention.service.MissionService;
 
 
@@ -12,29 +13,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/missions")
-public class MissionController {
-    @Autowired
-    MissionService ms;
+public class MissionController extends GenericController<Mission,Long,MissionService> {
 
-    @GetMapping("/{id}")
-    Mission getMissionById(@PathVariable long id){
-        return ms.getById(id);
-    }
+
 
     @PostMapping
     Mission addMission(@RequestBody Mission m){
         System.out.println(m.toString());
-        return ms.add(m);
+        return service.add(m);
     }
 
-    @GetMapping
-    List<Mission> getAll(){
-        return ms.getAll();
-    }
+
 
 
 
 }
- **/
